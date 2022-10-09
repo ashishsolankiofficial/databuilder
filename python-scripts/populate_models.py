45cf35af0b0c2d689c9cd1a9aa6b448bf43b3ad4
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2022-10-09 16:00:09 +0530

    database script user added

--- python-scripts/populate_models.py
+++ python-scripts/populate_models.py
@@ -1,9 +1,10 @@
+import email
 import os
 import pandas as pd
 from pepper.settings import BASE_DIR
 from utils.models import City, Country
-import pdb
 from restaurant.models import Cuisine, Restaurant
+from users.models import User, Role
 
 csv_file_path = os.path.join(os.path.dirname(BASE_DIR), "databuilder/python-scripts/file1.csv")
 
@@ -37,3 +38,28 @@ for i, row in df_master.iterrows():
     for c in row["cuisines"].split(', '):
         restaurant_cusines = Cuisine.objects.filter(name=c).first()
         restaurant.cuisines.add(restaurant_cusines)
+
+for i in range(4):
+    role = Role(id=i)
+    role.save()
+
+
+user = User(first_name='demo', last_name='manager', email='manager@pd.com')
+user.set_password('admin')
+user.save()
+user.roles.add(Role.objects.filter(id=0).first())
+
+user = User(first_name='demo', last_name='sales', email='sales@pd.com')
+user.set_password('admin')
+user.save()
+user.roles.add(Role.objects.filter(id=1).first())
+
+user = User(first_name='demo', last_name='inventory', email='inventory@pd.com')
+user.set_password('admin')
+user.save()
+user.roles.add(Role.objects.filter(id=2).first())
+
+user = User(first_name='demo', last_name='customer', email='customer@pd.com')
+user.set_password('admin')
+user.save()
+user.roles.add(Role.objects.filter(id=3).first())
